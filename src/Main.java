97c662e49db3f807f6789d73947a26b99dc4cd55
BASIPRANEETHKUMAR <basipraneethkumar2@gmail.com>
2026-04-01 14:33:20 +0530

    03 UC2 - Basic Room Types & Static Availability

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    /**
+     * Main method - entry point of the Java application.
+     * JVM starts execution from this method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Application name and version (String literals)
+        String appName = "Hotel Booking System";
+        String version = "v1.0";
+
+        // Print welcome message to console
+        System.out.println("Welcome to " + appName + " " + version + "!");
+        System.out.println("Application started successfully.");
+
+        // Program ends here (linear execution)
+    }
+}
